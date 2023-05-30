27c9ac31ad83dfc4fc13c2a070b7421e4a9c00d4
joavidbui <joavidbui@alu.edu.gva.es>
2023-05-30 18:29:51 +0200

    Polidivisible -> Code & Tests

--- Algoritmes/src/es/iesmz/ed/algoritmes/Polidivisible.java
+++ Algoritmes/src/es/iesmz/ed/algoritmes/Polidivisible.java
@@ -9,6 +9,15 @@ public class Polidivisible {
     }
 
     public boolean esPolidivisible() {
-        return false;
+        String numeroString = String.valueOf(Math.abs(this.numero));
+        boolean esPolidivisible = true;
+        for (int i = 1; i <= numeroString.length(); i++) {
+            long currentNumber = Long.parseLong(numeroString.substring(0, i));
+            if (currentNumber % i != 0) {
+                esPolidivisible = false;
+                break;
+            }
+        }
+        return esPolidivisible;
     }
 }
--- Algoritmes/src/es/iesmz/ed/algoritmes/PolidivisibleTest.java
+++ Algoritmes/src/es/iesmz/ed/algoritmes/PolidivisibleTest.java
@@ -14,14 +14,15 @@ class PolidivisibleTest {
     }
 
     @ParameterizedTest
-    @ValueSource(longs = {})
+    @ValueSource(longs = {1L, 2L, 3L, 4L, 10L, 102L, 1020L, 10200L, 345654L, 102000L, 1020005L, 10200056L, 102000564L,
+            381654729L, 1020005640L})
     void shouldReturnPolydivisible(long number) {
         polidivisible = new Polidivisible(number);
         Assertions.assertTrue(polidivisible.esPolidivisible());
     }
 
     @ParameterizedTest
-    @ValueSource(longs = {})
+    @ValueSource(longs = {11L, 125L, 1234L, 4897654L, 546857645L})
     void shouldReturnNOTPolydivisible(long number) {
         polidivisible = new Polidivisible(number);
         Assertions.assertFalse(polidivisible.esPolidivisible());
