eb44352e96dd04ec3b7df21ede599a4791de2737
joavidbui <joavidbui@alu.edu.gva.es>
2023-05-30 18:34:31 +0200

    Polidivisible -> Refactor and JavaDoc

--- Algoritmes/src/es/iesmz/ed/algoritmes/Polidivisible.java
+++ Algoritmes/src/es/iesmz/ed/algoritmes/Polidivisible.java
@@ -1,13 +1,24 @@
 package es.iesmz.ed.algoritmes;
 
+/**
+ * Clase con un atributo numérico long y una función para determinar si dicho número es polidivisible.
+ */
 public class Polidivisible {
 
-    private long numero;
+    private final long numero;
 
+    /**
+     * Constructor para el comprobador de que todos un número es polidivisible.
+     * @param numero el número sobre el que se realizará la comprobación.
+     */
     public Polidivisible(long numero) {
         this.numero = numero;
     }
 
+    /**
+     * Función que devuelve un boolean en función de si el atributo "número" del objeto es polidivisible.
+     * @return boolean
+     */
     public boolean esPolidivisible() {
         String numeroString = String.valueOf(Math.abs(this.numero));
         boolean esPolidivisible = true;
