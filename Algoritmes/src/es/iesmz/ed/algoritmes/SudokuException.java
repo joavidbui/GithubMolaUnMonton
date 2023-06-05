caec3bfca78de0a2bd024e014891e95700b40b0b
joavidbui <joavidbui@alu.edu.gva.es>
2023-06-05 14:02:22 +0200

    Sudoku -> Documentación de SudokuException.java

--- Algoritmes/src/es/iesmz/ed/algoritmes/SudokuException.java
+++ Algoritmes/src/es/iesmz/ed/algoritmes/SudokuException.java
@@ -1,5 +1,8 @@
 package es.iesmz.ed.algoritmes;
 
+/**
+ * Throwable que se usa para indicar un error en el constructor de Sudokus.
+ */
 public class SudokuException extends Throwable {
     public SudokuException(String message) {
         super(message);
