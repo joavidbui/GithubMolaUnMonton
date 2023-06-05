a743722249d65651626db63ba1eeae4736db0663
joavidbui <joavidbui@alu.edu.gva.es>
2023-06-05 12:45:01 +0200

    CalculMental -> documentacio

--- Algoritmes/src/es/iesmz/ed/algoritmes/CalculMental.java
+++ Algoritmes/src/es/iesmz/ed/algoritmes/CalculMental.java
@@ -2,13 +2,26 @@ package es.iesmz.ed.algoritmes;
 
 import java.util.ArrayList;
 
+/**
+ * Clase con un atributo String - que debe contener una serie de operaciones matemáticas con números naturales - y una
+ * función para realizar la serie de cálculos.
+ */
 public class CalculMental {
-    private String operacio;
+    private final String operacio;
 
+    /**
+     * Contructor de la clase CalculMental
+     * @param operacion Una sucesión de sumas y restas de números naturales en formato String.
+     */
     public CalculMental(String operacion) {
         this.operacio = operacion;
     }
 
+    /**
+     * Convierte la string almacenada en el objeto en una serie de números naturales, resultados sucesivos de las
+     * sumas y restas que ésta indica.
+     * @return ArrayList/<Integer/>
+     */
     public ArrayList<Integer> calcula() {
         ArrayList<Integer> solucio = new ArrayList<>();
         ArrayList<Integer> nombresEnters = new ArrayList<>();
