8ff660d9626442185d951d50a16e8e98a444a547
joavidbui <joavidbui@alu.edu.gva.es>
2023-06-05 13:04:06 +0200

    Sudoku -> 1er test i codi

--- Algoritmes/src/es/iesmz/ed/algoritmes/Sudoku.java
+++ Algoritmes/src/es/iesmz/ed/algoritmes/Sudoku.java
@@ -1,5 +1,7 @@
 package es.iesmz.ed.algoritmes;
 
+import java.util.ArrayList;
+
 public class Sudoku {
     int [][] matriuSudoku;
 
@@ -8,6 +10,15 @@ public class Sudoku {
     }
 
     public boolean esCorrecte() {
-        return false;
+        ArrayList<Integer> nombresUtilitzats = new ArrayList<>();
+
+        for (int i = 0; i < 9; i++) {
+            nombresUtilitzats.clear();
+            for (int j = 0; j < 9; j++) {
+                if (nombresUtilitzats.contains(matriuSudoku[i][j])) return false;
+                else nombresUtilitzats.add(matriuSudoku[i][j]);
+            }
+        }
+        return true;
     }
 }
--- Algoritmes/src/es/iesmz/ed/algoritmes/SudokuTest.java
+++ Algoritmes/src/es/iesmz/ed/algoritmes/SudokuTest.java
@@ -26,7 +26,7 @@ class SudokuTest {
     @ParameterizedTest
     @MethodSource("InputsSudokuEsCorrecteShouldReturnTrue")
         // six numbers
-    void sudokuEsCorrecteShouldReturnTrue(int[][] inputSudoku, ArrayList<Integer> expectedOutputs) {
+    void sudokuEsCorrecteShouldReturnTrue(int[][] inputSudoku) {
         sudoku = new Sudoku(inputSudoku);
         Assertions.assertTrue(sudoku.esCorrecte());
     }
@@ -44,6 +44,29 @@ class SudokuTest {
                         { 9, 7, 2, 3, 5, 8, 4, 6, 1 },
                         { 6, 3, 4, 2, 7, 1, 9, 5, 8 }})
                 );
+    }
+
+    @ParameterizedTest
+    @MethodSource("InputsSudokuEsCorrecteShouldReturnFalse")
+        // six numbers
+    void sudokuEsCorrecteShouldReturnFalse(int[][] inputSudoku) {
+        sudoku = new Sudoku(inputSudoku);
+        Assertions.assertFalse(sudoku.esCorrecte());
+    }
+
+    private static Stream<Arguments> InputsSudokuEsCorrecteShouldReturnFalse() {
+        return Stream.of(
+                Arguments.of((Object) new int[][]{
+                        { 4, 1, 3, 8, 2, 5, 6, 7, 9 },
+                        { 5, 6, 7, 1, 4, 9, 8, 3, 2 },
+                        { 2, 8, 9, 7, 3, 6, 1, 4, 5 },
+                        { 1, 9, 5, 4, 6, 2, 7, 8, 3 },
+                        { 7, 2, 6, 9, 8, 3, 5, 1, 4 },
+                        { 3, 4, 8, 5, 1, 7, 2, 9, 6 },
+                        { 8, 5, 1, 6, 9, 4, 3, 2, 7 },
+                        { 9, 7, 2, 3, 5, 8, 4, 6, 1 },
+                        { 6, 3, 4, 2, 7, 1, 9, 5, 8 }})
+        );
     }
 
 }
